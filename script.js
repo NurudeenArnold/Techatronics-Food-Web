f2bab21c4e287908bd85c0cf7f89a585c98b0f84
Nurudeen <nurudeenarnold@gmail.com>
2024-03-27 10:01:09 +0200

    Navbar Mobile Responsiveness

--- script.js
+++ script.js
@@ -175,3 +175,29 @@ function DisplayError(error) {
   });
   return;
 }
+
+/*-----------------------------------------Responsive Navbar-----------------------------------------*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    let top = window.scrollY;
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height){
+            navLinks.forEach (link => {
+                link.classList.remove('active');
+            });
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        }
+    });
+};
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
